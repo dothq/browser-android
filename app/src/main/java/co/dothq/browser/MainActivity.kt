f9bcd747bc84dc6925ad58c110446e9fce1dc156
Ejaz Ali <ejazali4@protonmail.ch>
2021-12-28 23:29:19 +0000

    ✨ Changed Engine-Gecko for GeckoView

--- app/src/main/java/co/dothq/browser/MainActivity.kt
+++ app/src/main/java/co/dothq/browser/MainActivity.kt
@@ -2,10 +2,23 @@ package co.dothq.browser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.mozilla.geckoview.GeckoRuntime;
+import org.mozilla.geckoview.GeckoSession;
+import org.mozilla.geckoview.GeckoView;
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view = findViewById<GeckoView>(R.id.geckoview)
+        val session = GeckoSession()
+        val runtime = GeckoRuntime.create(this)
+
+        session.open(runtime)
+        view.setSession(session)
+        session.loadUri("https://dothq.co")
     }
 }
